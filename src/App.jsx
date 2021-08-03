82b5f20accea0fa1d02bca2a708eaef04f7c7fee
정재용 <wodydthk7@hanyang.ac.kr>
2021-08-03 15:20:18 +0900

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import { StrictMode } from 'react';
+import styles from './App.module.css';
+
+
+function App() {
+  return (
+    <StrictMode>
+      <div className={styles.App}>
+        <h1>Hello World</h1>
+      </div>
+    </StrictMode>
+  );
+}
+
+export default App;
