e6eb1fa071709953af2a4019552642180e0d1d18
정재용 <wodydthk7@hanyang.ac.kr>
2021-08-14 13:24:32 +0900

    update watch view UI

--- src/components/watchView/watchView.jsx
+++ src/components/watchView/watchView.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
 import styles from './watchView.module.css'
 
-const WatchView = ({content}) => {
+const WatchView = ({content, content:{snippet, contentDetails, statistics}}) => {
+  const {title, description, thumbnails, channelTitle, channelThumbnails, publishedAt} = snippet
+  const {duration} = contentDetails
+  const {viewCount} = statistics
+
   return ( 
     <div className={styles.watch_container}>
-
       <iframe className={styles.video} 
         type="text/html" 
         src={`https://www.youtube.com/embed/${content ? content.id : ''}`}
         allowFullScreen autoplay="1"/>
-      {content &&
-        <div>  
-          <div>
-            <h1>title</h1>
-            <p>meta</p>
-          </div>
+  
+      <div className={styles.details}>  
+        <div className={styles.meta}>
+          <h1 className={styles.title}>{title}</h1>
+          <div className={styles.info_block}>
+            <p className={styles.meta_line}><span className={styles.view_count}>{viewCount}</span>{publishedAt}</p>
+            <div className={styles.menu}>
 
-          <div>
-            <div>
-              <img src="" alt="" />
-              <div>
-                <h1>channel</h1>
-                <p>channel info</p>
-              </div>
-              <button>subscribe</button>
-            </div>
-            <div>
-              <p>description</p>
             </div>
           </div>
-
         </div>
-      }
+
+        <div className={styles.description}>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <h1>channel</h1>
+              <p>channel info</p>
+            </div>
+            <button>subscribe</button>
+          </div>
+          <div>
+            <p>description</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
